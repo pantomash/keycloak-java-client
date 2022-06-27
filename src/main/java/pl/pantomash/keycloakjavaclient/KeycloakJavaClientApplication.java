6232d715f02e0e0839fc2753d2b05a6c22026441
tomasz.piotrowicz <tomasz.piotrowicz@amano.ai>
2022-06-27 18:42:21 +0200

    prepare starter spring boot configuration

---
+++ src/main/java/pl/pantomash/keycloakjavaclient/KeycloakJavaClientApplication.java
@@ -0,0 +1,13 @@
+package pl.pantomash.keycloakjavaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakJavaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakJavaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pantomash/keycloakjavaclient/KeycloakJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pantomash.keycloakjavaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class KeycloakJavaClientApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
